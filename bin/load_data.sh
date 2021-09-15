3cf38386e34936aa5e16649adc265d2d8ac8ffa2
David McQueen <david.mcqueen@neudesic.com>
2021-09-15 11:43:33 -0700

    adding more ranking methods

--- bin/load_data.sh
+++ bin/load_data.sh
@@ -1,4 +1,4 @@
 for file in $(ls data/*.json); do  \
   java -jar bin/vespa-http-client-jar-with-dependencies.jar \
-    --verbose --file $file --endpoint http://localhost:8080; \
+    --verbose --file $file --endpoint http://localhost:8080; 
 done
