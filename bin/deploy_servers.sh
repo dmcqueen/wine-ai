d6e340f2c03c9efb0da453efb9a78ea8f087a736
David McQueen <dmcqueen@gmail.com>
2025-07-08 22:08:44 +0700

    cleanup

--- bin/deploy_servers.sh
+++ bin/deploy_servers.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-docker build tensor_server -t modelserver
-docker run --detach --name models --hostname models-container --publish 8088:8088 modelserver
-
 if ! docker network ls --format '{{.Name}}' | grep -wq vespa-net; then
     docker network create vespa-net
 fi
 
+docker build tensor_server -t modelserver
+docker run --detach --name models --network vespa-net --publish 8088:8088 modelserver
 docker run --detach --name vespa --network vespa-net --publish 8080:8080 --publish 19071:19071 vespaengine/vespa
